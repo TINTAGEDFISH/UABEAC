5c5a4c3af89aae0d9e63c1eb93104a1192ba34e5
tintagedfish <1234567890@163.com>
2026-05-24 19:41:43 +0800

    图标

--- UABEAvalonia/Forms/AssetTypeIconConverter.cs
+++ UABEAvalonia/Forms/AssetTypeIconConverter.cs
@@ -22,65 +22,65 @@ namespace UABEAvalonia
             {
                 if ((int)assetClass < 0)
                 {
-                    return GetBitmap("UABEAvalonia/Assets/Icons/asset-mono-behaviour.png");
+                    return GetBitmap("Assets/Icons/asset-mono-behaviour.png");
                 }
 
                 return assetClass switch
                 {
-                    AssetClassID.Animation => GetBitmap("UABEAvalonia/Assets/Icons/asset-animation.png"),
-                    AssetClassID.AnimationClip => GetBitmap("UABEAvalonia/Assets/Icons/asset-animation-clip.png"),
-                    AssetClassID.Animator => GetBitmap("UABEAvalonia/Assets/Icons/asset-animator.png"),
-                    AssetClassID.AnimatorController => GetBitmap("UABEAvalonia/Assets/Icons/asset-animator-controller.png"),
-                    AssetClassID.AnimatorOverrideController => GetBitmap("UABEAvalonia/Assets/Icons/asset-animator-override-controller.png"),
-                    AssetClassID.AudioClip => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-clip.png"),
-                    AssetClassID.AudioListener => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-listener.png"),
-                    AssetClassID.AudioMixer => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-mixer.png"),
-                    AssetClassID.AudioMixerGroup => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-mixer-group.png"),
-                    AssetClassID.AudioSource => GetBitmap("UABEAvalonia/Assets/Icons/asset-audio-source.png"),
-                    AssetClassID.Avatar => GetBitmap("UABEAvalonia/Assets/Icons/asset-avatar.png"),
-                    AssetClassID.BillboardAsset => GetBitmap("UABEAvalonia/Assets/Icons/asset-billboard.png"),
-                    AssetClassID.BillboardRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-billboard-renderer.png"),
-                    AssetClassID.BoxCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-box-collider.png"),
-                    AssetClassID.Camera => GetBitmap("UABEAvalonia/Assets/Icons/asset-camera.png"),
-                    AssetClassID.Canvas => GetBitmap("UABEAvalonia/Assets/Icons/asset-canvas.png"),
-                    AssetClassID.CanvasGroup => GetBitmap("UABEAvalonia/Assets/Icons/asset-canvas-group.png"),
-                    AssetClassID.CanvasRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-canvas-renderer.png"),
-                    AssetClassID.CapsuleCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-capsule-collider.png"),
-                    AssetClassID.CapsuleCollider2D => GetBitmap("UABEAvalonia/Assets/Icons/asset-capsule-collider.png"),
-                    AssetClassID.ComputeShader => GetBitmap("UABEAvalonia/Assets/Icons/asset-compute-shader.png"),
-                    AssetClassID.Cubemap => GetBitmap("UABEAvalonia/Assets/Icons/asset-cubemap.png"),
-                    AssetClassID.Flare => GetBitmap("UABEAvalonia/Assets/Icons/asset-flare.png"),
-                    AssetClassID.FlareLayer => GetBitmap("UABEAvalonia/Assets/Icons/asset-flare-layer.png"),
-                    AssetClassID.Font => GetBitmap("UABEAvalonia/Assets/Icons/asset-font.png"),
-                    AssetClassID.GameObject => GetBitmap("UABEAvalonia/Assets/Icons/asset-game-object.png"),
-                    AssetClassID.Light => GetBitmap("UABEAvalonia/Assets/Icons/asset-light.png"),
-                    AssetClassID.LightmapSettings => GetBitmap("UABEAvalonia/Assets/Icons/asset-lightmap-settings.png"),
-                    AssetClassID.LODGroup => GetBitmap("UABEAvalonia/Assets/Icons/asset-lod-group.png"),
-                    AssetClassID.Material => GetBitmap("UABEAvalonia/Assets/Icons/asset-material.png"),
-                    AssetClassID.Mesh => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh.png"),
-                    AssetClassID.MeshCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-collider.png"),
-                    AssetClassID.MeshFilter => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-filter.png"),
-                    AssetClassID.MeshRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-renderer.png"),
-                    AssetClassID.MonoBehaviour => GetBitmap("UABEAvalonia/Assets/Icons/asset-mono-behaviour.png"),
-                    AssetClassID.MonoScript => GetBitmap("UABEAvalonia/Assets/Icons/asset-mono-script.png"),
-                    AssetClassID.NavMeshSettings => GetBitmap("UABEAvalonia/Assets/Icons/asset-nav-mesh-settings.png"),
-                    AssetClassID.ParticleSystem => GetBitmap("UABEAvalonia/Assets/Icons/asset-particle-system.png"),
-                    AssetClassID.ParticleSystemRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-particle-system-renderer.png"),
-                    AssetClassID.RectTransform => GetBitmap("UABEAvalonia/Assets/Icons/asset-rect-transform.png"),
-                    AssetClassID.ReflectionProbe => GetBitmap("UABEAvalonia/Assets/Icons/asset-reflection-probe.png"),
-                    AssetClassID.Rigidbody => GetBitmap("UABEAvalonia/Assets/Icons/asset-rigidbody.png"),
-                    AssetClassID.Shader => GetBitmap("UABEAvalonia/Assets/Icons/asset-shader.png"),
-                    AssetClassID.ShaderVariantCollection => GetBitmap("UABEAvalonia/Assets/Icons/asset-shader-collection.png"),
-                    AssetClassID.SkinnedMeshRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-mesh-renderer.png"), // todo
-                    AssetClassID.Sprite => GetBitmap("UABEAvalonia/Assets/Icons/asset-sprite.png"),
-                    AssetClassID.SpriteRenderer => GetBitmap("UABEAvalonia/Assets/Icons/asset-sprite-renderer.png"),
-                    AssetClassID.Terrain => GetBitmap("UABEAvalonia/Assets/Icons/asset-terrain.png"),
-                    AssetClassID.TerrainCollider => GetBitmap("UABEAvalonia/Assets/Icons/asset-terrain-collider.png"),
-                    AssetClassID.TextAsset => GetBitmap("UABEAvalonia/Assets/Icons/asset-text-asset.png"),
-                    AssetClassID.Texture2D => GetBitmap("UABEAvalonia/Assets/Icons/asset-texture2d.png"),
-                    AssetClassID.Texture3D => GetBitmap("UABEAvalonia/Assets/Icons/asset-texture2d.png"),
-                    AssetClassID.Transform => GetBitmap("UABEAvalonia/Assets/Icons/asset-transform.png"),
-                    _ => GetBitmap("UABEAvalonia/Assets/Icons/asset-unknown.png"),
+                    AssetClassID.Animation => GetBitmap("Assets/Icons/asset-animation.png"),
+                    AssetClassID.AnimationClip => GetBitmap("Assets/Icons/asset-animation-clip.png"),
+                    AssetClassID.Animator => GetBitmap("Assets/Icons/asset-animator.png"),
+                    AssetClassID.AnimatorController => GetBitmap("Assets/Icons/asset-animator-controller.png"),
+                    AssetClassID.AnimatorOverrideController => GetBitmap("Assets/Icons/asset-animator-override-controller.png"),
+                    AssetClassID.AudioClip => GetBitmap("Assets/Icons/asset-audio-clip.png"),
+                    AssetClassID.AudioListener => GetBitmap("Assets/Icons/asset-audio-listener.png"),
+                    AssetClassID.AudioMixer => GetBitmap("Assets/Icons/asset-audio-mixer.png"),
+                    AssetClassID.AudioMixerGroup => GetBitmap("Assets/Icons/asset-audio-mixer-group.png"),
+                    AssetClassID.AudioSource => GetBitmap("Assets/Icons/asset-audio-source.png"),
+                    AssetClassID.Avatar => GetBitmap("Assets/Icons/asset-avatar.png"),
+                    AssetClassID.BillboardAsset => GetBitmap("Assets/Icons/asset-billboard.png"),
+                    AssetClassID.BillboardRenderer => GetBitmap("Assets/Icons/asset-billboard-renderer.png"),
+                    AssetClassID.BoxCollider => GetBitmap("Assets/Icons/asset-box-collider.png"),
+                    AssetClassID.Camera => GetBitmap("Assets/Icons/asset-camera.png"),
+                    AssetClassID.Canvas => GetBitmap("Assets/Icons/asset-canvas.png"),
+                    AssetClassID.CanvasGroup => GetBitmap("Assets/Icons/asset-canvas-group.png"),
+                    AssetClassID.CanvasRenderer => GetBitmap("Assets/Icons/asset-canvas-renderer.png"),
+                    AssetClassID.CapsuleCollider => GetBitmap("Assets/Icons/asset-capsule-collider.png"),
+                    AssetClassID.CapsuleCollider2D => GetBitmap("Assets/Icons/asset-capsule-collider.png"),
+                    AssetClassID.ComputeShader => GetBitmap("Assets/Icons/asset-compute-shader.png"),
+                    AssetClassID.Cubemap => GetBitmap("Assets/Icons/asset-cubemap.png"),
+                    AssetClassID.Flare => GetBitmap("Assets/Icons/asset-flare.png"),
+                    AssetClassID.FlareLayer => GetBitmap("Assets/Icons/asset-flare-layer.png"),
+                    AssetClassID.Font => GetBitmap("Assets/Icons/asset-font.png"),
+                    AssetClassID.GameObject => GetBitmap("Assets/Icons/asset-game-object.png"),
+                    AssetClassID.Light => GetBitmap("Assets/Icons/asset-light.png"),
+                    AssetClassID.LightmapSettings => GetBitmap("Assets/Icons/asset-lightmap-settings.png"),
+                    AssetClassID.LODGroup => GetBitmap("Assets/Icons/asset-lod-group.png"),
+                    AssetClassID.Material => GetBitmap("Assets/Icons/asset-material.png"),
+                    AssetClassID.Mesh => GetBitmap("Assets/Icons/asset-mesh.png"),
+                    AssetClassID.MeshCollider => GetBitmap("Assets/Icons/asset-mesh-collider.png"),
+                    AssetClassID.MeshFilter => GetBitmap("Assets/Icons/asset-mesh-filter.png"),
+                    AssetClassID.MeshRenderer => GetBitmap("Assets/Icons/asset-mesh-renderer.png"),
+                    AssetClassID.MonoBehaviour => GetBitmap("Assets/Icons/asset-mono-behaviour.png"),
+                    AssetClassID.MonoScript => GetBitmap("Assets/Icons/asset-mono-script.png"),
+                    AssetClassID.NavMeshSettings => GetBitmap("Assets/Icons/asset-nav-mesh-settings.png"),
+                    AssetClassID.ParticleSystem => GetBitmap("Assets/Icons/asset-particle-system.png"),
+                    AssetClassID.ParticleSystemRenderer => GetBitmap("Assets/Icons/asset-particle-system-renderer.png"),
+                    AssetClassID.RectTransform => GetBitmap("Assets/Icons/asset-rect-transform.png"),
+                    AssetClassID.ReflectionProbe => GetBitmap("Assets/Icons/asset-reflection-probe.png"),
+                    AssetClassID.Rigidbody => GetBitmap("Assets/Icons/asset-rigidbody.png"),
+                    AssetClassID.Shader => GetBitmap("Assets/Icons/asset-shader.png"),
+                    AssetClassID.ShaderVariantCollection => GetBitmap("Assets/Icons/asset-shader-collection.png"),
+                    AssetClassID.SkinnedMeshRenderer => GetBitmap("Assets/Icons/asset-mesh-renderer.png"),
+                    AssetClassID.Sprite => GetBitmap("Assets/Icons/asset-sprite.png"),
+                    AssetClassID.SpriteRenderer => GetBitmap("Assets/Icons/asset-sprite-renderer.png"),
+                    AssetClassID.Terrain => GetBitmap("Assets/Icons/asset-terrain.png"),
+                    AssetClassID.TerrainCollider => GetBitmap("Assets/Icons/asset-terrain-collider.png"),
+                    AssetClassID.TextAsset => GetBitmap("Assets/Icons/asset-text-asset.png"),
+                    AssetClassID.Texture2D => GetBitmap("Assets/Icons/asset-texture2d.png"),
+                    AssetClassID.Texture3D => GetBitmap("Assets/Icons/asset-texture2d.png"),
+                    AssetClassID.Transform => GetBitmap("Assets/Icons/asset-transform.png"),
+                    _ => GetBitmap("Assets/Icons/asset-unknown.png"),
                 };
             }
 
@@ -90,19 +90,22 @@ namespace UABEAvalonia
 
         Dictionary<string, Bitmap> cache = new();
 
-        private Bitmap GetBitmap(string path)
+        private Bitmap GetBitmap(string relativePath)
         {
-            Bitmap? bitmap;
-            if (cache.TryGetValue(path, out bitmap))
+            if (cache.TryGetValue(relativePath, out var bitmap))
             {
                 return bitmap;
             }
-            else
-            {
-                bitmap = new Bitmap(AssetLoader.Open(new Uri($"avares://{path}")));
-                cache[path] = bitmap;
-                return bitmap;
-            }
+
+            // ensure no leading slash
+            relativePath = relativePath.TrimStart('/');
+
+            // build avares uri using this assembly's name so it survives AssemblyName changes
+            string asmName = typeof(AssetTypeIconConverter).Assembly.GetName().Name ?? "UABEAvalonia";
+            var uri = new Uri($"avares://{asmName}/{relativePath}");
+            bitmap = new Bitmap(AssetLoader.Open(uri));
+            cache[relativePath] = bitmap;
+            return bitmap;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
